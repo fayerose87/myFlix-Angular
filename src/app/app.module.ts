e598e1ad0cd57e7e1005a57a4f4c584b14dfb731
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-11-28 19:28:20 -0600

    Add welcome page and rrouting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,19 +13,31 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
-    MovieCardComponent
+    MovieCardComponent,
+    WelcomePageComponent
   ],
   imports: [
+    MatIconModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
@@ -36,7 +48,8 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
     MatFormFieldModule,
     MatDialogModule,
     MatSnackBarModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -15,27 +16,32 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    public router: Router
   ) { }
 
   ngOnInit(): void {
   }
 
-  // function for sending form inputs to backend 
-  loginUser(): void {
-    this.fetchApiData.userLogin(this.userData).subscribe((response) => {
-      // [[ logic for successful user login goes here! TBI ]]
-      this.dialogRef.close(); //close the modal on success
-      console.log(response);
-      this.snackBar.open('User logged in successfully!', 'OK', {
-        duration: 2000
-      });
-    }, (response) => {
-      console.log(response);
-      this.snackBar.open(response, 'OK', {
-        duration: 2000
-      });
+// function for sending form inputs to backend 
+loginUser(): void {
+  this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+    localStorage.setItem('token', response.token);
+    localStorage.setItem('user', response.user.Username);
+    // [[ logic for successful user login goes here! TBI ]]
+    this.dialogRef.close(); //close the modal on success
+    console.log(response);
+    this.snackBar.open('User logged in successfully!', 'OK', {
+      duration: 2000
     });
-  }
+  }, (response) => {
+    console.log(response);
+    this.snackBar.open(response, 'OK', {
+      duration: 2000
+    });
+  });
+  this.router.navigate(['movies']);
+
+}
 
 }
